14d695d6b17f829e145b9c11c92e5aa2a7fa2346
ChristopherRush <chris.uclan@gmail.com>
2020-05-26 15:27:35 +0100

    Create smartremote_fix_max2play.sh

---
+++ smartremote_fix_max2play.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Create a file to modify the value corresponding to the OK and Menu button
+sudo su
+cat > /lib/udev/hwdb.d/70-local-keyboard.hwdb << EOF
+evdev:input:b0003v2252p0120*
+ KEYBOARD_KEY_c0041=enter
+ KEYBOARD_KEY_c0040=c
+EOF
+
+# Update the hardware database
+udevadm hwdb --update && udevadm trigger -s input
